1b1f984c69c3e5e12167372e1f9624d3b76b5791
George <122010570+georgepata98@users.noreply.github.com>
2024-07-05 21:25:34 +0300

    Update autofit.C

--- autofit.C
+++ autofit.C
@@ -1,25 +1,30 @@
 {
   #include <string>
 
+  TCanvas *c1 = nullptr;
   TString fileName;
   fstream ofile;
   Float_t particleMass, x1, x2, meanPt, meanPtErr;
+  Int_t n;  //hist or graph
 
 
-  cout << "Enter the name of .root file: "; cin >> fileName;
+
+  cout << endl << "Choose (1 or 2): " << endl << "1. TH1 (hist)" << endl << "2. TGraphErrors (graph)" << endl;
+  cin >> n;
+  cout << "Enter the name of .root file: "; 
+  cin >> fileName;
 
   TFile *tfile = TFile::Open(fileName + ".root");
   if(!tfile || tfile->IsZombie())
   {
     return 1;
   }
-
-  TCanvas *c1 = nullptr;
   TIter next(tfile->GetListOfKeys());
   TKey *key;
 
-  //fitting functions
-  TF1 *fitFunc[5][2];  //
+
+  //----------FITTING FUNCTIONS----------
+  TF1 *fitFunc[5][2];  //5 lines, 2 columns
   TF1 *meanFunc[5][2], *meanFunc2[5][2];
 
   fitFunc[0][0] = new TF1("fitFunc_11", "[0]/6.283/[1]/[1]*exp(-x/[1])", 0, 1.5);
@@ -34,73 +39,118 @@
   fitFunc[4][1] = new TF1("fitFunc_52", "[0]*exp(-x*x/[2]/[2])+[3]*exp(-x*x/[4]/[4])", 0, 2);
 
 
-
   vector<TH1F*> histArray;
+
+  if(n==1)  //histograms
+  {
+    while((key = (TKey*)next()))
+    {
+      TObject *obj = key->ReadObj();
+      TH1 *hist = dynamic_cast<TH1*>(obj);
+      histArray.push_back(dynamic_cast<TH1F*>(hist));  //filling histArray vector
+    }
+  }
+
+
   vector<TGraphErrors*> graphArray;
 
-  TGraphErrors *graph;
-
-  while((key = (TKey*)next()))
+  if(n==2)  //graphs with errors
   {
-    TObject *obj = key->ReadObj();
-    TH1 *hist = dynamic_cast<TH1*>(obj);
-    histArray.push_back(dynamic_cast<TH1F*>(hist));  //filling histArray vector
-  }
-
-  for(size_t i=0; i<histArray.size(); ++i)  //filling graphArray with histArray
-  {
-    graph = new TGraphErrors(histArray[i]->GetNbinsX());
-    for(Int_t j=1; j<=histArray[i]->GetNbinsX(); j++)
+    while((key = (TKey*)next()))
     {
-      Float_t x = histArray[i]->GetBinCenter(j);
-      Float_t y = histArray[i]->GetBinContent(j);
-      Float_t ex = histArray[i]->GetBinWidth(j)/2;
-      Float_t ey = histArray[i]->GetBinError(j);
+      if(key->IsFolder())
+      {
+        TDirectory *dir = (TDirectory*)key->ReadObj();
+        dir->cd();
 
-      graph->SetPoint(j-1, x, y);
-      graph->SetPointError(j-1, ex, ey);
+        TList *subKeyList = dir->GetListOfKeys();
+        TIter subNext(subKeyList);
+        TKey *subKey;
+
+        while((subKey = (TKey*)subNext()))
+        {
+          TGraphErrors *graph = (TGraphErrors*)subKey->ReadObj();
+          graphArray.push_back(graph);
+        }
+      }
     }
-    graphArray.push_back(graph);
-    delete graph;
   }
+
 
 
 
 
   
-  //Data writing
+  //----------DATA WRITING----------
   ofile.open("output_data.txt", ios::out);  //write data to .txt file
 
-  for(size_t i=0; i<histArray.size(); ++i)
+  if(n==1)  //histograms
   {
-    cout << endl << "Enter the mass for '" << histArray[i]->GetName() << "' : "; cin >> particleMass;
-    for(Int_t j=0; j<=4; j++)  //loop over all fit functions
+    for(size_t i=0; i<histArray.size(); ++i)
     {
-      if(j==0 || j==4) continue;
-      for(Int_t k=0; k<=1; k++)  //loop over the two fitting intervals: (0,1.5) and (0,2)
+      cout << endl << "Enter the mass for '" << histArray[i]->GetName() << "' : "; cin >> particleMass;
+      for(Int_t j=0; j<=4; j++)  //loop over all fit functions
       {
-        x1=0;
-        if(k==0) x2=1.5;
-        if(k==1) x2=2.0;
+        if(j==0 || j==4) continue;  //fara prima si ultima functie de fit
+        for(Int_t k=0; k<=1; k++)  //loop over the two fitting intervals (0,1.5) and (0,2)
+        {
+          x1=0;
+          if(k==0) x2=1.5;
+          if(k==1) x2=2.0;
 
-        fitFunc[j][k]->FixParameter(1, particleMass);
-        fitFunc[j][k]->SetParameter(0, 200);
-        fitFunc[j][k]->SetParameter(2, 0.2);
+          fitFunc[j][k]->FixParameter(1, particleMass);
+          fitFunc[j][k]->SetParameter(0, 200);
+          fitFunc[j][k]->SetParameter(2, 0.2);
 
-        histArray[i]->Fit(fitFunc[j][k], "Q0", "", x1, x2);
-        //graphArray[i]->Fit(fitFunc[j][k], "Q0", "", x1, x2);
+          histArray[i]->Fit(fitFunc[j][k], "Q0", "", x1, x2);
 
-        meanFunc[j][k] = new TF1("mean", ("6.283 * x * fitFunc_" + std::to_string(j+1) + std::to_string(k+1)).c_str(), 0, 10);
-        meanFunc2[j][k] = new TF1("mean2", ("6.283 * x * x * fitFunc_" + std::to_string(j+1) + std::to_string(k+1)).c_str(), 0, 10);
+          meanFunc[j][k] = new TF1("mean", ("6.283 * x * fitFunc_" + std::to_string(j+1) + std::to_string(k+1)).c_str(), 0, 10);
+          meanFunc2[j][k] = new TF1("mean2", ("6.283 * x * x * fitFunc_" + std::to_string(j+1) + std::to_string(k+1)).c_str(), 0, 10);
 
-        meanPt = meanFunc2[j][k]->Integral(0, 10) / meanFunc[j][k]->Integral(0, 10);
-        meanPtErr = (meanFunc2[j][k]->Integral(0, 10) / meanFunc[j][k]->Integral(0, 10)) * sqrt(( meanFunc2[j][k]->IntegralError(0, 10) * meanFunc2[j][k]->IntegralError(0, 10)) / (meanFunc2[j][k]->Integral(0, 10) * meanFunc2[j][k]->Integral(0, 10)) + (meanFunc[j][k]->IntegralError(0, 10) * meanFunc[j][k]->IntegralError(0, 10)) / (meanFunc[j][k]->Integral(0, 10) * meanFunc[j][k]->Integral(0, 10)));
+          meanPt = meanFunc2[j][k]->Integral(0, 10) / meanFunc[j][k]->Integral(0, 10);
+          meanPtErr = (meanFunc2[j][k]->Integral(0, 10) / meanFunc[j][k]->Integral(0, 10)) * sqrt((meanFunc2[j][k]->IntegralError(0, 10) * meanFunc2[j][k]->IntegralError(0, 10)) / (meanFunc2[j][k]->Integral(0, 10) * meanFunc2[j][k]->Integral(0, 10)) + (meanFunc[j][k]->IntegralError(0, 10) * meanFunc[j][k]->IntegralError(0, 10)) / (meanFunc[j][k]->Integral(0, 10) * meanFunc[j][k]->Integral(0, 10)));
 
 
-        ofile << histArray[i]->GetName() << "   " << ("fit_function" + std::to_string(j+1)).c_str() << "   " << "(" << x1 << "," << x2 << ")" << "   " << fitFunc[j][k]->GetParameter(0) << "+/-" << fitFunc[j][k]->GetParError(0) << "   " << fitFunc[j][k]->GetParameter(1) << "   " << fitFunc[j][k]->GetParameter(2) << "+/-" << fitFunc[j][k]->GetParError(2) << "   " << meanPt << "+/-" << meanPtErr << "   " << fitFunc[j][k]->GetChisquare() << "/" << fitFunc[j][k]->GetNDF() << endl;
+          ofile << histArray[i]->GetName() << "   " << ("fit_function" + std::to_string(j+1)).c_str() << "   " << "(" << x1 << "," << x2 << ")" << "   " << fitFunc[j][k]->GetParameter(0) << "+/-" << fitFunc[j][k]->GetParError(0) << "   " << fitFunc[j][k]->GetParameter(1) << "   " << fitFunc[j][k]->GetParameter(2) << "+/-" << fitFunc[j][k]->GetParError(2) << "   " << meanPt << "+/-" << meanPtErr << "   " << fitFunc[j][k]->GetChisquare() << "/" << fitFunc[j][k]->GetNDF() << endl;
+        }
       }
     }
   }
+
+
+  if(n==2)  //graphs with errors
+  {
+    for(size_t i=0; i<graphArray.size(); ++i)
+    {
+      cout << endl << "Enter the mass for '" << graphArray[i]->GetName() << "' : "; cin >> particleMass;
+      for(Int_t j=0; j<=4; j++)  //loop over all fit functions
+      {
+        if(j==0 || j==4) continue;  //fara prima si ultima functie de fit
+        for(Int_t k=0; k<=1; k++)  //loop over the two fitting intervals (0,1.5) and (0,2)
+        {
+          x1=0;
+          if(k==0) x2=1.5;
+          if(k==1) x2=2.0;
+
+          fitFunc[j][k]->FixParameter(1, particleMass);
+          fitFunc[j][k]->SetParameter(0, 200);
+          fitFunc[j][k]->SetParameter(2, 0.2);
+
+          graphArray[i]->Fit(fitFunc[j][k], "Q0", "", x1, x2);
+
+          meanFunc[j][k] = new TF1("mean", ("6.283 * x * fitFunc_" + std::to_string(j+1) + std::to_string(k+1)).c_str(), 0, 10);
+          meanFunc2[j][k] = new TF1("mean2", ("6.283 * x * x * fitFunc_" + std::to_string(j+1) + std::to_string(k+1)).c_str(), 0, 10);
+
+          meanPt = meanFunc2[j][k]->Integral(0, 10) / meanFunc[j][k]->Integral(0, 10);
+          meanPtErr = (meanFunc2[j][k]->Integral(0, 10) / meanFunc[j][k]->Integral(0, 10)) * sqrt((meanFunc2[j][k]->IntegralError(0, 10) * meanFunc2[j][k]->IntegralError(0, 10)) / (meanFunc2[j][k]->Integral(0, 10) * meanFunc2[j][k]->Integral(0, 10)) + (meanFunc[j][k]->IntegralError(0, 10) * meanFunc[j][k]->IntegralError(0, 10)) / (meanFunc[j][k]->Integral(0, 10) * meanFunc[j][k]->Integral(0, 10)));
+
+
+          ofile << graphArray[i]->GetName() << "   " << ("fit_function" + std::to_string(j+1)).c_str() << "   " << "(" << x1 << "," << x2 << ")" << "   " << fitFunc[j][k]->GetParameter(0) << "+/-" << fitFunc[j][k]->GetParError(0) << "   " << fitFunc[j][k]->GetParameter(1) << "   " << fitFunc[j][k]->GetParameter(2) << "+/-" << fitFunc[j][k]->GetParError(2) << "   " << meanPt << "+/-" << meanPtErr << "   " << fitFunc[j][k]->GetChisquare() << "/" << fitFunc[j][k]->GetNDF() << endl;
+        }
+      }
+    }
+  }
+
 
   ofile.close();
   tfile->Close();
